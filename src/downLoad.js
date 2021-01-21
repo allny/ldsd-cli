79115e075e4a1faff193a57314bb7f3a234426df
Aanll <245568831@qq.com>
2021-01-21 12:06:21 +0800

    修改完善脚手架

--- src/downLoad.js
+++ src/downLoad.js
@@ -1,23 +1,21 @@
-//git包
-const downLoad = require("download-git-repo");
-//动画
-const ora = require("ora");
-let url = "http://gitlab.leidishidai.com/ldsd-common-system/ldsd-system-ui";
-let clone = false;
-let downGit = name => {
-  const spinner = ora("正在拉取模板...");
-  spinner.start();
-  downLoad(
-    url,
-    name,
-    {
-      clone
-    },
-    err => {
-      spinner.stop();
-      console.log(err ? err : "项目创建成功");
-      process.exit(1);
-    }
-  );
-};
-module.exports = downGit;
+// git包
+const downLoad = require('download-git-repo')
+// 动画
+const ora = require('ora')
+
+
+let url = 'zhou1591/zjs-template'
+let clone = false
+let downGit = (name) => {
+  const spinner = ora('正在拉取模板...')
+  spinner.start()
+  downLoad(url, name, {
+    clone
+  }, err => {
+    spinner.stop()
+    console.log(err?err:"项目创建成功")
+    process.exit(1)
+  })
+
+}
+module.exports = downGit
